315c00c4ebe8eebebc03fc86f02313d01dfd50b4
Ludovicamassimino <ludovica.massimino93@gmail.com>
2024-03-17 20:37:56 +0100

    add other comments

--- jsnack4/js4/app.js
+++ jsnack4/js4/app.js
@@ -6,16 +6,19 @@ console.log('ciao')
 const invitati = ["Andrea"  ,  "Eleonora" ,  "Lucia"  ,  "Francesco"  ,  "Anna"  ,  "Enrico"  ,  "Lorenzo"];
 console.log('Nomi invitati: ' + invitati)
 
-
+// chiedo all'utente il suo nome:
 const nomeUtente = prompt('Inserisci il tuo nome')
 console.log('Nome utente: ' + nomeUtente)
 
+// cerco il nome che ha inserito l'utente all'interno dell'array 'invitati' utilizzando indexOf:
 const cercaNome = invitati.indexOf(nomeUtente)
 console.log(cercaNome)
 
-
+//scrivo la condizione SE indexOf da un risultato >0 (cioè ha trovato il suo nome nell'array) allora comunico all'utente che può partecipare:
 if(cercaNome > 0) {
     console.log('Puoi partecipare!')
+
+    // ALTRIMENTI gli comunico che non può partecipare (indexOf restituisce -1, cioè non ha trovato il nome nell'array di nomi)
 }else{
     console.log('Non puoi partecipare')
 }
