9dc523cb80dcc2c6fb7a5ca3082645375c7c0439
Ludovicamassimino <ludovica.massimino93@gmail.com>
2024-03-17 20:28:09 +0100

    non riesce

---
+++ jsnack3/js3/app.js
@@ -0,0 +1,27 @@
+console.log('ciao')
+
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+
+
+
+
+
+// creo un array vuoto
+const numeri = [];
+
+// uso ciclo for per chiedere per 10 volte all'utente di inserire un numero parseInt((prompt)):
+for(let i = 0; i < 10; i++){
+    const numero = parseInt(prompt('inserisci un numero'));
+    
+    numeri.push(numero)
+    console.log(numeri)
+    // divido i numeri per sommarli tra loro:
+        const numeriSplit = parseInt(numeri.split)
+        console.log(numeriSplit)
+
+    // sommo i numeri all'interno dell'array:
+    
+    // const somma = numero+numero;
+    // console.log(somma)
+}
