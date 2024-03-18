8313d3247d5838739630e2f3094cf761000ef596
Ludovicamassimino <ludovica.massimino93@gmail.com>
2024-03-18 19:18:11 +0100

    tabellina done

---
+++ jsnack7/js7/app.js
@@ -0,0 +1,13 @@
+console.log('ciao')
+
+// Scrivere un programma che stampi la tabellina del 2, fino al numero 1000.
+
+// Modificate poi il programma in modo che venga chiesto all'utente il numero massimo consentito, e stampare tutta la tabellina del 2 fino al numero inserito.
+
+
+// scrivo la tabellina del 2 all'interno del ciclo for perchè la moltiplicazione si deve ripetere fino ad arrivare al risultato 1000:
+for (let i = 1; i < 501; i++){
+    const tabellina = 2 * i
+    console.log(tabellina)
+}
+
