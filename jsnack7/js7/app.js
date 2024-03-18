ff9ae06919fe76e946c0dea6bbb43555f96ccb7e
Ludovicamassimino <ludovica.massimino93@gmail.com>
2024-03-18 19:37:10 +0100

    2nd step done

--- jsnack7/js7/app.js
+++ jsnack7/js7/app.js
@@ -6,8 +6,21 @@ console.log('ciao')
 
 
 // scrivo la tabellina del 2 all'interno del ciclo for perchè la moltiplicazione si deve ripetere fino ad arrivare al risultato 1000:
+/*
 for (let i = 1; i < 501; i++){
     const tabellina = 2 * i
     console.log(tabellina)
-}
+} 
+*/
 
+// chiedo all'utente fino a che numero vuole che venga calcolata e stampata la tabellina:
+
+const numero = parseInt(prompt('Inserisci il numero massimo consentito'))
+console.log("L'utente vuole che la tabellina venga calcolata fino al: " + numero)
+
+// scrivo la tabellina del 2 all'interno del ciclo for perchè la moltiplicazione si deve calcolare tante volte quante il numero scelto dall'utente:
+
+for (let i = 0; i < (numero+1); i++){
+    const tabellina = 2 * i
+    console.log('2 * ' + i + ' = ' + tabellina)
+}
